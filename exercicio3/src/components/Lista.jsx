77eab22e6c4dc53ebb8efea4a8967ef4ee762f80
Rirfit <lilrirfit@gmail.com>
2024-10-20 10:57:22 -0300

    FC

---
+++ exercicio3/src/components/Lista.jsx
@@ -0,0 +1,12 @@
+function Lista(){
+    return (
+        <>
+        <ol>
+            <li>Atividade do jaja(Proa)</li>
+            <li>Atividade do Furia (faculdade)</li>
+            <li>Tomar banho</li>
+        </ol>
+        </>
+
+    )
+} export default Lista
